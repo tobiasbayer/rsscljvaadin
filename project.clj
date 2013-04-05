36dc793132d3eeacf9fd8fa8aa3b2d6a2dea14a3
Tobias Bayer <tobias.bayer@inovex.de>
2013-04-05 12:58:39 +0200

    Clean project definition

--- project.clj
+++ project.clj
@@ -1,11 +1,7 @@
-(defproject rsscljvaadin "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-		 [com.vaadin/vaadin-server "7.0.1"]
-                 [com.vaadin/vaadin-client-compiled "7.0.1"]
-                 [com.vaadin/vaadin-themes "7.0.1"]
+(defproject rsscljvaadin "1.0.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.5.0"]
+		 [com.vaadin/vaadin-server "7.0.3"]
+                 [com.vaadin/vaadin-client-compiled "7.0.3"]
+                 [com.vaadin/vaadin-themes "7.0.3"]
                  [javax.servlet/servlet-api "2.5"]]
   :aot [rsscljvaadin.rssapplicationui])
